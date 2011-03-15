45ff5d015d07644c4c640c84507d961bc56c663f
Chris <churley1@umd.edu>
2011-03-14 23:44:37 -0400

    More tests for SWTWindow

--- src/edu/umd/cs/guitar/ripper/test/SWTWindowTest.java
+++ src/edu/umd/cs/guitar/ripper/test/SWTWindowTest.java
@@ -1,5 +1,6 @@
 package edu.umd.cs.guitar.ripper.test;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import org.eclipse.swt.SWT;
@@ -94,5 +95,62 @@ public class SWTWindowTest {
 		
 		display.dispose();
 	}
-
+	
+	@Test
+	public void testEquals(){
+		Display display = new Display();
+		Shell shell1 = new Shell(display);
+		SWTWindow window1 = new SWTWindow(shell1);
+		
+		assertTrue(window1.equals(window1));
+		
+		assertFalse(window1.equals(shell1));
+		
+		assertFalse(window1.equals(null));
+		
+		Shell shell2 = new Shell(display);
+		SWTWindow window2 = new SWTWindow(shell2);
+		
+		assertTrue(window1.equals(window2));
+		
+		SWTWindow nullShell = new SWTWindow(null);
+		assertFalse(nullShell.equals(window1));
+		
+		Shell shell3 = new Shell(display);
+		shell3.setText("Window");
+		SWTWindow window3 = new SWTWindow(shell3);
+		assertFalse(window1.equals(window3));
+		
+		display.dispose();
+	}
+	
+	@Test
+	public void testGetContainer(){
+		Display display = new Display();
+		Shell shell1 = new Shell(display);
+		SWTWindow window1 = new SWTWindow(shell1);
+		
+		window1.getContainer();
+		display.dispose();
+	}
+	
+	@Test
+	public void testGetGUIProperties(){
+		Display display = new Display();
+		Shell shell1 = new Shell(display);
+		SWTWindow window1 = new SWTWindow(shell1);
+		
+		window1.getGUIProperties();
+		display.dispose();
+	}
+	
+	@Test
+	public void testExtractGUIProperties(){
+		Display display = new Display();
+		Shell shell1 = new Shell(display);
+		SWTWindow window1 = new SWTWindow(shell1);
+		
+		window1.extractGUIProperties();
+		display.dispose();
+	}
 }
