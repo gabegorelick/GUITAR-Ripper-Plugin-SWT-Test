11e124c7e9539cea663753670fded5184ac3bcb6
Gabe Gorelick <gabegorelick@gmail.com>
2011-03-15 00:11:09 -0400

    Clean up Chris's additions to SWTWindowTest.

--- src/edu/umd/cs/guitar/ripper/test/SWTWindowTest.java
+++ src/edu/umd/cs/guitar/ripper/test/SWTWindowTest.java
@@ -1,7 +1,8 @@
 package edu.umd.cs.guitar.ripper.test;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -11,15 +12,15 @@ import org.junit.Test;
 import edu.umd.cs.guitar.model.SWTWindow;
 
 public class SWTWindowTest {
-	
+
 	@Test
 	public void testEmptyShell() {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.getShell(), shell);
-		
+
 		display.dispose();
 	}
 
@@ -27,11 +28,16 @@ public class SWTWindowTest {
 	public void testShellTitle() {
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setText("ShellTitle");
+
+		assertEquals(shell.getClass().getName(),
+				new SWTWindow(shell).getTitle());
+
+		String shellTitle = "ShellTitle";
+		shell.setText(shellTitle);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
-		assertEquals(swtWindow.getTitle(), "ShellTitle");
-		
+
+		assertEquals(shellTitle, swtWindow.getTitle());
+
 		display.dispose();
 	}
 
@@ -41,10 +47,10 @@ public class SWTWindowTest {
 		Shell shell = new Shell(display);
 		shell.setLocation(1, 1);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.getX(), 1);
 		assertEquals(swtWindow.getY(), 1);
-		
+
 		display.dispose();
 	}
 
@@ -55,9 +61,9 @@ public class SWTWindowTest {
 		shell.setText("ShellTitle");
 		shell.setVisible(true);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.isValid(), true);
-		
+
 		display.dispose();
 	}
 
@@ -68,9 +74,9 @@ public class SWTWindowTest {
 		shell.setText("ShellTitle");
 		shell.setVisible(false);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.isValid(), false);
-		
+
 		display.dispose();
 	}
 
@@ -79,9 +85,9 @@ public class SWTWindowTest {
 		Display display = new Display();
 		Shell shell = new Shell(display, SWT.APPLICATION_MODAL);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.isModal(), true);
-		
+
 		display.dispose();
 	}
 
@@ -90,66 +96,66 @@ public class SWTWindowTest {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		SWTWindow swtWindow = new SWTWindow(shell);
-		
+
 		assertEquals(swtWindow.isModal(), false);
-		
+
 		display.dispose();
 	}
-	
+
 	@Test
-	public void testEquals(){
+	public void testEquals() {
 		Display display = new Display();
 		Shell shell1 = new Shell(display);
 		SWTWindow window1 = new SWTWindow(shell1);
-		
+
 		assertTrue(window1.equals(window1));
-		
+
 		assertFalse(window1.equals(shell1));
-		
+
 		assertFalse(window1.equals(null));
-		
+
 		Shell shell2 = new Shell(display);
 		SWTWindow window2 = new SWTWindow(shell2);
-		
+
 		assertTrue(window1.equals(window2));
-		
+
 		SWTWindow nullShell = new SWTWindow(null);
 		assertFalse(nullShell.equals(window1));
-		
+
 		Shell shell3 = new Shell(display);
 		shell3.setText("Window");
 		SWTWindow window3 = new SWTWindow(shell3);
 		assertFalse(window1.equals(window3));
-		
+
 		display.dispose();
 	}
-	
+
 	@Test
-	public void testGetContainer(){
+	public void testGetContainer() {
 		Display display = new Display();
 		Shell shell1 = new Shell(display);
 		SWTWindow window1 = new SWTWindow(shell1);
-		
+
 		window1.getContainer();
 		display.dispose();
 	}
-	
+
 	@Test
-	public void testGetGUIProperties(){
+	public void testGetGUIProperties() {
 		Display display = new Display();
 		Shell shell1 = new Shell(display);
 		SWTWindow window1 = new SWTWindow(shell1);
-		
+
 		window1.getGUIProperties();
 		display.dispose();
 	}
-	
+
 	@Test
-	public void testExtractGUIProperties(){
+	public void testExtractGUIProperties() {
 		Display display = new Display();
 		Shell shell1 = new Shell(display);
 		SWTWindow window1 = new SWTWindow(shell1);
-		
+
 		window1.extractGUIProperties();
 		display.dispose();
 	}
