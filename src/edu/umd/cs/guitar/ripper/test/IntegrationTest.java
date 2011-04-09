c4eac9a83608b96b4132be083cd2ee81d232a273
Prime <Prime@192.168.1.3>
2011-04-09 17:30:18 -0400

    xmlunit added to integration tests

--- src/edu/umd/cs/guitar/ripper/test/IntegrationTest.java
+++ src/edu/umd/cs/guitar/ripper/test/IntegrationTest.java
@@ -1,18 +1,33 @@
 package edu.umd.cs.guitar.ripper.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.custommonkey.xmlunit.NodeDetail;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import edu.umd.cs.guitar.ripper.SWTRipper;
 import edu.umd.cs.guitar.ripper.SWTRipperConfiguration;
 import edu.umd.cs.guitar.ripper.SWTRipperRunner;
+
 
 public class IntegrationTest {
 
@@ -27,8 +42,36 @@ public class IntegrationTest {
 		new SWTRipperRunner(swtRipper).start();
 				
 		String name = "expected/" + filename + ".xml";
-		assertEquals(-1, diff(name, config.getGuiFile()));
-
+		//assertEquals(-1, diff(name, config.getGuiFile()));
+		
+		Document result = buildDoc(config.getGuiFile());
+		Document test = buildDoc(name);
+		DetailedDiff diff = new DetailedDiff(new Diff(test,result));
+		
+		List<Difference> difference = diff.getAllDifferences();
+		if(difference.size() == 0 || difference == null){
+			assertTrue(diff.similar());
+		}
+		else{
+			int diffcount = 0;
+			for(Difference d: difference){
+				NodeDetail nd = d.getTestNodeDetail();
+				Node parent = nd.getNode().getParentNode().getParentNode();
+				NodeList nl = parent.getChildNodes();
+				for(int i = 0; i < nl.getLength(); i++){
+					if(nl.item(i).getNodeName().equals("Name")){
+						String text = nl.item(i).getFirstChild().getNodeValue();
+						System.out.println(text.toString()); ////
+						if(text.equals("X") || text.equals("Y") || text.equals("text") ){
+							diffcount++;
+						}
+						System.out.println(d.toString()); ////
+					}
+				}
+			}
+			assertEquals(difference.size(),diffcount);
+		}
+				
 	}
 
 	/**
@@ -49,27 +92,38 @@ public class IntegrationTest {
 		}
 
 		int lineNumber = 1;
-		String s1 = new String();
-		String s2 = new String();
-
+		String expectedString;
+		String actualString;
+		
+		String lastLine = null;
+		
 		boolean equal = true;
-		try {
+		try { // TODO this doesn't work if one of the files is empty
 			while (expectedReader.ready()) {
-				s1 = expectedReader.readLine();
+				expectedString = expectedReader.readLine();
 				if (actualReader.ready()) {
-					s2 = actualReader.readLine();
+					actualString = actualReader.readLine();
 				} else {
 					equal = false;
 					break;
 				}
-				if (!s1.equals(s2)) {
-					System.err.println("Failed at line " + lineNumber);
-					System.err.println("Expected: " + s1);
-					System.err.println("Actual: " + s2);
-					equal = false;
-					break;
+				
+				// ignore if this is X or Y value
+				// TODO use XMLUnit to do this
+				if (!expectedString.equals(actualString)) {
+					if (lastLine != null 
+							&& !lastLine.contains("<Name>X</Name>")
+							&& !lastLine.contains("<Name>Y</Name>")) {
+					
+						System.err.println("Failed at line " + lineNumber);
+						System.err.println("Expected: " + expectedString);
+						System.err.println("Actual: " + actualString);
+						equal = false;
+						break;
+					}
 				}
 				lineNumber++;
+				lastLine = actualString;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -81,6 +135,24 @@ public class IntegrationTest {
 			return lineNumber;
 		}
 
+	}
+	
+	private static Document buildDoc(String filename){
+		Document doc = null;
+		try {
+			File file = new File(filename);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			doc = db.parse(file);
+			
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return doc;
 	}
 
 	@Test
@@ -120,12 +192,17 @@ public class IntegrationTest {
 
 	@Test
 	public void testTwoWindowsApp() {
-		ripAndDiff("SWTTwoWindowsApp"); // FIXME can't rip subcomponents of multi-window apps
+		ripAndDiff("SWTTwoWindowsApp");
 	}
 
 	@Test
 	public void testWindowApp() {
 		ripAndDiff("SWTWindowApp");
 	}
+	
+//	@Test
+//	public void testMultiWindowDynamicApp() {
+//		ripAndDiff("SWTMultiWindowDynamicApp");
+//	}
 
 }
