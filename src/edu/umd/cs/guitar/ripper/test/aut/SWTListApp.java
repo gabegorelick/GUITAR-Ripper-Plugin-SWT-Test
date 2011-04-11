6760cb6d8f21e013f9da43ff2bc21b0476c1e38f
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-11 02:59:43 -0400

    Make SWTListApp cross-platform

--- src/edu/umd/cs/guitar/ripper/test/aut/SWTListApp.java
+++ src/edu/umd/cs/guitar/ripper/test/aut/SWTListApp.java
@@ -3,8 +3,15 @@ package edu.umd.cs.guitar.ripper.test.aut;
 //Credit to http://zetcode.com/tutorials/javaswttutorial/widgets/
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
 
 public class SWTListApp {
 	private Shell shell;
@@ -39,7 +46,7 @@ public class SWTListApp {
 
 		status.setLayoutData(labelData);
 
-		final List list = new List(shell, SWT.BORDER);
+		final List list = new List(shell, SWT.BORDER | SWT.SINGLE);
 
 		list.add("Item 1");
 		list.add("Item 2");
@@ -47,17 +54,25 @@ public class SWTListApp {
 		list.add("Item 4");
 		list.add("Item 5");
 
-		list.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e) {
-				String[] items = list.getSelection();
-				status.setText(items[0]);
-			}
-		});
-
 		FormData listData = new FormData();
 		listData.left = new FormAttachment(shell, 30, SWT.LEFT);
 		listData.top = new FormAttachment(shell, 30, SWT.TOP);
 		list.setLayoutData(listData);
+		
+		list.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				status.setText(list.getSelection()[0]);
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// do nothing
+			}
+		});
+				
+		// for some reason calling this causes the label to be "Ready" on GTK 
+		list.select(0);
 	}
 
 	public static void main(String[] args) {
