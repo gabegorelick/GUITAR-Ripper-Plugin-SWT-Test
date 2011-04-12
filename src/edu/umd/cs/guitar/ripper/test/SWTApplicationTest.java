1f7b6b4aa90b20045ea3ed497f81218f3122b547
Prime <Prime@192.168.1.3>
2011-04-12 00:08:44 -0400

    getAllWindows workaround

--- src/edu/umd/cs/guitar/ripper/test/SWTApplicationTest.java
+++ src/edu/umd/cs/guitar/ripper/test/SWTApplicationTest.java
@@ -2,13 +2,17 @@ package edu.umd.cs.guitar.ripper.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.junit.Test;
 
 import edu.umd.cs.guitar.model.SWTApplication;
+import edu.umd.cs.guitar.model.SWTApplicationStartException;
 import edu.umd.cs.guitar.ripper.SWTRipper;
 import edu.umd.cs.guitar.ripper.SWTRipperConfiguration;
 import edu.umd.cs.guitar.ripper.SWTRipperRunner;
 import edu.umd.cs.guitar.ripper.test.aut.SWTBasicApp;
+import edu.umd.cs.guitar.ripper.test.aut.SWTTwoWindowsApp;
 
 public class SWTApplicationTest {
 	
@@ -26,12 +30,24 @@ public class SWTApplicationTest {
 	
 	@Test
 	public void testGetAllWindow() {
-		SWTRipperConfiguration config = new SWTRipperConfiguration();
-		config.setMainClass(TEST_CLASS_NAME);
-		SWTRipper ripper = new SWTRipper(config, Thread.currentThread());
-		new SWTRipperRunner(ripper).start();
 		
-		// TODO figure out how to test this if AUT is closed by this point
-//		ripper.getMonitor().getApplication().getAllWindow();
+		SWTRipperConfiguration config = new SWTRipperConfiguration();
+		config.setMainClass(SWTTwoWindowsApp.class.getName());
+		SWTRipper ripper = new SWTRipper(config, Thread.currentThread());
+		
+		
+		Display display = new Display();
+		Shell shell1 = new Shell(display);
+		Shell shell2 = new Shell(display);
+		SWTApplication swtapp = ripper.getMonitor().getApplication();
+		try {
+			swtapp.startGUI();
+			assertEquals(2,swtapp.getAllWindow().size());
+		} catch (SWTApplicationStartException e) {
+			//This will get thrown
+		}
+		
+		//new SWTRipperRunner(ripper).start();
+		
 	}
 }
