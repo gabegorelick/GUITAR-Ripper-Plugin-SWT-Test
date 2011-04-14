18c71ee96eb247bd9bec281e03ef8128534c4723
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-14 04:28:14 -0400

    Delete event stubs

--- src/edu/umd/cs/guitar/ripper/test/SWTCompositeTest.java
+++ src/edu/umd/cs/guitar/ripper/test/SWTCompositeTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Hashtable;
-import java.util.List;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
@@ -16,8 +13,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.umd.cs.guitar.event.SWTEventHandler;
-import edu.umd.cs.guitar.model.GComponent;
 import edu.umd.cs.guitar.model.GUITARConstants;
 import edu.umd.cs.guitar.model.SWTWindow;
 import edu.umd.cs.guitar.model.swtwidgets.SWTComposite;
@@ -177,26 +172,5 @@ public class SWTCompositeTest {
 //		manager.registerEvent(SWTEventHandlerStub.class);
 //		comp.getEventList();
 //		manager.getEvents().clear();
-	}
-	
-	// no available constructor for getEventList since it's private
-	// TODO mock this instead
-	private class MyEventNoConstructor extends SWTEventHandler {
-
-		@Override
-		public boolean isSupportedBy(GComponent arg0) {
-			return true;
-		}
-
-		@Override
-		protected void performImpl(GComponent arg0,
-				Hashtable<String, List<String>> arg1) {		
-		}
-
-		@Override
-		protected void performImpl(GComponent arg0, Object arg1,
-				Hashtable<String, List<String>> arg2) {
-		}
-		
 	}
 }
