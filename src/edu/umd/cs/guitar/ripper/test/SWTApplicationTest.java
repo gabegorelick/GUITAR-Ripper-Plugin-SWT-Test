8df4b9bf90e4c15b38a6d79f5bae99060a5f3439
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-14 02:55:19 -0400

    Merge in Aaron's changes to SWTApplicationTest by hand

--- src/edu/umd/cs/guitar/ripper/test/SWTApplicationTest.java
+++ src/edu/umd/cs/guitar/ripper/test/SWTApplicationTest.java
@@ -2,17 +2,34 @@ package edu.umd.cs.guitar.ripper.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.umd.cs.guitar.model.SWTApplication;
 import edu.umd.cs.guitar.ripper.SWTRipper;
 import edu.umd.cs.guitar.ripper.SWTRipperConfiguration;
-import edu.umd.cs.guitar.ripper.SWTRipperRunner;
 import edu.umd.cs.guitar.ripper.test.aut.SWTBasicApp;
 
 public class SWTApplicationTest {
 	
 	private final static String TEST_CLASS_NAME = SWTBasicApp.class.getName();
+	
+	private Display display;
+	
+	@Before
+	public void setUp() {
+		if (display == null || display.isDisposed()) {
+			display = new Display();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		display.dispose();
+	}
 	
 	/**
 	 * Test that the application is set to run on the <code>main</code> thread.
@@ -29,9 +46,26 @@ public class SWTApplicationTest {
 		SWTRipperConfiguration config = new SWTRipperConfiguration();
 		config.setMainClass(TEST_CLASS_NAME);
 		SWTRipper ripper = new SWTRipper(config, Thread.currentThread());
-		new SWTRipperRunner(ripper).start();
 		
-		// TODO figure out how to test this if AUT is closed by this point
-//		ripper.getMonitor().getApplication().getAllWindow();
+		Shell shell1 = new Shell(display);
+		Shell shell2 = new Shell(display);
+		shell1.setVisible(true);
+		shell2.setVisible(true);
+		SWTApplication app = ripper.getMonitor().getApplication();
+		app.connect();
+		
+		assertEquals(2, app.getAllWindow().size());
+		
+		shell2.dispose();
+		assertEquals(1, app.getAllWindow().size());
+		
+		shell1.setVisible(false);
+		assertEquals(0, app.getAllWindow().size());
+		
+		shell1.setVisible(true);
+		assertEquals(1, app.getAllWindow().size());
+		
+		shell1.dispose();
+		assertEquals(0, app.getAllWindow().size());
 	}
 }
