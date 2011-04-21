c025b9c228999c8f0e6ce8f4104966280476e05d
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-20 20:49:56 -0400

    New test: SWTWidgetFactoryTest

---
+++ src/edu/umd/cs/guitar/ripper/test/SWTWidgetFactoryTest.java
@@ -0,0 +1,38 @@
+package edu.umd.cs.guitar.ripper.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.umd.cs.guitar.model.swtwidgets.SWTDecorations;
+import edu.umd.cs.guitar.model.swtwidgets.SWTWidgetFactory;
+
+
+public class SWTWidgetFactoryTest {
+
+	private Display display;
+	SWTWidgetFactory factory;
+	
+	@Before
+	public void setUp() {
+		display = new Display();
+		factory = SWTWidgetFactory.INSTANCE;
+	}
+	
+	@After
+	public void tearDown() {
+		display.dispose();
+	}
+	
+	@Test
+	public void testGetWidgetAdapter() {		
+		Shell shell = new Shell(display);
+		
+		assertEquals(SWTDecorations.class, factory.getWidgetAdapter(shell));
+	}
+		
+}
