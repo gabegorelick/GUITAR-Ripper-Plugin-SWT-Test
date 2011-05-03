5fdf88655f6d98ea9e22289f7858d0dde9ee1328
Gabe Gorelick <gabegorelick@gmail.com>
2011-05-03 01:07:21 -0400

    Update TuxGuitarTest

--- src/edu/umd/cs/guitar/ripper/test/TuxGuitarTest.java
+++ src/edu/umd/cs/guitar/ripper/test/TuxGuitarTest.java
@@ -1,22 +1,66 @@
 package edu.umd.cs.guitar.ripper.test;
 
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
 import org.herac.tuxguitar.gui.TGMain;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import edu.umd.cs.guitar.ripper.SWTGuitarRunner;
-import edu.umd.cs.guitar.ripper.SWTRipper;
-import edu.umd.cs.guitar.ripper.SWTRipperConfiguration;
+import edu.umd.cs.guitar.model.SitarWindow;
+import edu.umd.cs.guitar.model.swtwidgets.SitarControl;
+import edu.umd.cs.guitar.model.swtwidgets.SitarDecorations;
+import edu.umd.cs.guitar.model.swtwidgets.SitarWidgetFactory;
+import edu.umd.cs.guitar.ripper.SitarRipper;
+import edu.umd.cs.guitar.ripper.SitarRipperConfiguration;
+import edu.umd.cs.guitar.ripper.SitarRunner;
 
 
 public class TuxGuitarTest {
 
 	@Test
+	@Ignore
 	public void testRipTuxGuitar() {
-		SWTRipperConfiguration config = new SWTRipperConfiguration();
+		SitarRipperConfiguration config = new SitarRipperConfiguration();
 		config.setInitialWaitTime(5000);
 		config.setMainClass(TGMain.class.getName());
 		config.setGuiFile("testoutput.xml");
-		SWTRipper ripper = new SWTRipper(config);
-		new SWTGuitarRunner(ripper).run();
+		SitarRipper ripper = new SitarRipper(config);
+		
+		SitarWidgetFactory.INSTANCE.registerWidgetAdapter(Button.class, TuxButton.class);
+		SitarWidgetFactory.INSTANCE.registerWidgetAdapter(Shell.class, TuxShell.class);
+		
+		new SitarRunner(ripper).run();
+	}
+	
+	public static class TuxButton extends SitarControl {
+
+		public TuxButton(Button button, SitarWindow window) {
+			super(button, window);
+		}
+		
+		@Override
+		protected void notifyAllListeners() {
+			// do nothing
+		}
+		
+	}
+		
+	public static class TuxShell extends SitarDecorations {
+		
+		private final boolean shouldOverride;
+		
+		public TuxShell(Shell widget, SitarWindow window) {
+			super(widget, window);
+			shouldOverride = getTitle().equals("TuxGuitar - Untitled.tg"); 
+		}
+		
+		@Override
+		public void notifyAllListeners() {
+			if (!shouldOverride) {
+				super.notifyAllListeners();
+			}
+		}
+		
 	}
 }
