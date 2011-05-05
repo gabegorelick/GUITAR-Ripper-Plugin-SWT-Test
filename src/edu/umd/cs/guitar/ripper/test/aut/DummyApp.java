e99d20eb9041133ca3142675915403ccb2a209ae
Gabe Gorelick <gabegorelick@gmail.com>
2011-05-05 04:20:56 -0400

    New DummyApp that doesn't block

---
+++ src/edu/umd/cs/guitar/ripper/test/aut/DummyApp.java
@@ -0,0 +1,14 @@
+package edu.umd.cs.guitar.ripper.test.aut;
+
+/**
+ * A sample application that doesn't do anything.
+ * 
+ * @author Gabe Gorelick
+ * 
+ */
+public class DummyApp {
+
+	public static void main(String[] args) {
+		
+	}
+}
